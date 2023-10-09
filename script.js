5f945606f6ea14d93f61b4e8dbad784fde313e3f
Zakaria Muse <zaknvf@Zakarias-MacBook-Air.local>
2023-10-09 16:03:23 -0500

    changing paragraphs

---
+++ script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    console.log("test");
+    $("#minimize").click(size);
+
+    function size() {
+    $(".footer").css("font-size", "7px");
+    $(".footnote").css("font-size", "10px");
+} 
+
+
+
+});
+
